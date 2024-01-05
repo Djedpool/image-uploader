095d0db2cf259b466915a851f9b8c385cc6af70f
Djedpool <oljacici@gmail.com>
2024-01-05 15:10:38 +0100

    Adding vue trough vite configuration

--- resources/views/images/index.blade.php
+++ resources/views/images/index.blade.php
@@ -5,10 +5,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Image Uploading with Vuew + Laravel</title>
     <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
+    @vite(['resources/js/app.js'])
 </head>
 <body>
-    <div class="max-w-lg mx-auto mt-24">
+    <div id="app" class="max-w-lg mx-auto mt-24">
         <h1 class="text-4xl font-bold text-center">Image Uploader</h1>
+        <counter />
     </div>
 </body>
 </html>
