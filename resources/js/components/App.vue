ecf59ff0f7100162ff8316e4d76d595392a923ae
Djedpool <oljacici@gmail.com>
2024-01-06 20:52:08 +0100

    Adding preview gallery and adding axios for vue

--- resources/js/components/App.vue
+++ resources/js/components/App.vue
@@ -1,19 +1,31 @@
 <template>
-    <div class="mt-4">
-        <file-pond
-            name="image"
-            ref="pond"
-            label-idle="Click to choose image, or drag here ..."
-            @init="filepondInitialized"
-            :allow-browse="false"
-            accepted-file-types="image/*"
-        />
+    <div>
+        <div class="mt-4">
+            <file-pond
+                name="image"
+                ref="pond"
+                label-idle="Click to choose image, or drag here ..."
+                @init="filepondInitialized"
+                :allow-browse="false"
+                accepted-file-types="image/*"
+                @processfile="handleProcessedFile"
+            />
+        </div>
+        <div class="mt-8 mb-24">
+            <h3 class="text-2xl font-medium text-center">Image Gallery</h3>
+            <div class="grid grid-cols-3 gap-2 justify-evenly mt-4">
+                <div v-for="(image, index) in images" :key="index">
+                    <img :src="'/storage/images/' + image" alt="">
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 <script>
 import vueFilePond, {setOptions} from 'vue-filepond'
 import 'filepond/dist/filepond.min.css'
 import FilePondPluginFileValidateType from 'filepond-plugin-file-validate-type'
+import axios from 'axios'
 
 setOptions({
     server: {
@@ -34,13 +46,31 @@ export default {
     },
     data() {
         return {
-
+            images: []
         }
+    },
+    mounted() {
+        axios.get('/images')
+            .then((response) => {
+                this.images = response.data
+                console.log(this.images)
+            })
+            .catch((error) => {
+                console.error(error)
+            })
     },
     methods: {
         filepondInitialized() {
             console.log('Filepond is ready!')
             console.log('Filepond object', this.$refs.pond)
+        },
+        handleProcessedFile(error, file) {
+            if (error) {
+                console.log(error)
+                return
+            }
+
+            this.images.unshift(file.serverId)
         }
     }
 }
