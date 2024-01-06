f8d3d88ad1fabf1cb7efadc8becd44b2612091f6
Djedpool <oljacici@gmail.com>
2024-01-06 20:19:16 +0100

    Adding image store functionality

--- app/Http/Controllers/ImageController.php
+++ app/Http/Controllers/ImageController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Image;
 use Illuminate\Http\Request;
 
 class ImageController extends Controller
@@ -16,11 +17,30 @@ class ImageController extends Controller
         // return all images
     }
 
-    public function store()
+    public function store(Request $request)
     {
-        // validate the incoming file
-        // save the file in storage
-        // create image model
-        // return that image model back to frontend
+        if(!$request->hasFile('image')) {
+            return response()->json(['error' => 'There is no image present.', 400]);
+        }
+
+        $request->validate(([
+            'image' => 'required|file|image'
+        ]));
+
+        $path = $request->file('image')->store('public/images');
+
+        if(!$path) {
+            return response()->json(['error' => 'The file could not be saved.'], 500);
+        }
+        
+        $uploadedFile = $request->file('image');
+
+        $image = Image::create([
+            'name' => $uploadedFile->hashName(),
+            'extension' => $uploadedFile->extension(),
+            'size' => $uploadedFile->getSize()
+        ]);
+        
+        return $image;
     }
 }
